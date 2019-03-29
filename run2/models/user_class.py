c9e6626bb0aa906e38d6c1f9db16dc1cbce750b4
Noah Schumacher <noahschumacher14@gmail.com>
2019-03-29 14:55:31 -0700

    Building a user class file.

---
+++ run2/models/user_class.py
@@ -0,0 +1,49 @@
+## This file creates a user object and get relevant
+## for creating a model.
+
+
+class User():
+
+	## Build the user class with just the ID
+	def __init__(self, user_id):
+		self.user_id = user_id
+
+
+	def build_table():
+		'''
+		Function: Builds the users meal ingredient table
+		- Format:
+			- row = meal
+			- cols = ingredients shown to customer (at any point in time)
+			- vals = 0, 1 (igredient present or not present)
+			- target = # time meal ordered / # times meal shown
+
+		Return:
+		-------
+		- 2D numpy array with data
+		'''
+
+
+	def _ingredients():
+		'''
+		Function: Gets the unique ingredients shown
+		to a user.
+		'''
+		pass
+
+	def _num_orders():
+		'''
+		Function: Gets the total number of orders for user
+		'''
+		pass
+
+
+	def _meal_ids():
+		'''
+		Function: Get the unqiue meal ids, 
+		'''
+		pass
+
+
+
+
