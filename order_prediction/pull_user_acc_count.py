7b9259c63bb37af7e882ba31b5240605f542efa3
Noah Schumacher <noahschumacher14@gmail.com>
2019-04-02 09:16:29 -0700

    added file the pulls user_id, account_id, total order count from db and pickles the dataframe.

---
+++ order_prediction/pull_user_acc_count.py
@@ -0,0 +1,24 @@
+
+
+import pandas as pd
+import numpy as np
+import pickle
+
+
+## local run sql file
+from db.python_db import connect, run_sql_query
+
+
+conn = connect()
+
+## Query gets cols (contact_sfid, contact_account_sfid, order_count)
+q = '''
+SELECT contact_sfid, contact_account_sfid, COUNT(contact_sfid) AS order_count
+FROM bi.executed_order_employee
+WHERE order_type = 'single'
+GROUP BY contact_sfid, contact_account_sfid
+ORDER BY order_count DESC'''
+
+user_account_order = run_sql_query(q, conn)
+
+pickle.dump(user_account_order, open('order_prediction/user_account_order.p', 'wb'))
