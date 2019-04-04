fe9bac8c4bcd71d4a30955a68e4109c1587c5a59
Noah Schumacher <noahschumacher14@gmail.com>
2019-04-04 12:41:03 -0700

    Updated meal test with more meals and plots.

--- order_prediction/test_meal.py
+++ order_prediction/test_meal.py
@@ -7,6 +7,9 @@ import numpy as np
 import pandas as pd
 import pickle
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 ## local run sql file
 from db.python_db import connect, run_sql_query
 
@@ -76,116 +79,100 @@ def get_score(meals, user_objs, ing_ids, real):
 	print("GB error: ", gb_error)
 	print("Avg error:", avg_error)
 
-	return rf_error, gb_error, avg_error
+	return ([rf_error, gb_error, avg_error], [rf_percents, gb_percents, real_p])
+
+
+def plot_swarm(rf_resids, gb_resids, base_resids):
+	## Converting residual information into dictionary for Swarm Plot
+	length = len(rf_resids*4)
+	models = ['Random Forest']*length + ['Gradient Boosted']*length + ['Base (.25)']*length
+	meals = [1,2,3,4]*(len(rf_resids)*3)
+
+
+	## Flattening and converting all residuals into lists
+	rf_rs = list(np.array(np.matrix(rf_resids).ravel())[0])
+	gb_rs = list(np.array(np.matrix(gb_resids).ravel())[0])
+	base_rs = list(np.array(np.matrix(base_resids).ravel())[0])
+
+	rs = rf_rs + gb_rs + base_rs
+
+	d = {'Model':models, 'Residual':rs, 'Meal':meals}
+	df = pd.DataFrame(d)
+
+	plt.figure(figsize=(8,4))
+	sns.set_context("talk")
+	sns.swarmplot(x="Residual", y="Model", data=df, size=6)
+
+	plt.vlines(np.mean(rf_rs), .2, 1.8, linestyles='--', color='steelblue', label="Mean {0:.3f}".format(np.mean(rf_rs)))
+	plt.vlines(np.mean(gb_rs), .2, 1.8, linestyles='--', color='mediumseagreen', label="Mean {0:.3f}".format(np.mean(gb_rs)))
+	plt.vlines(np.mean(base_rs), .2, 1.8, linestyles='--', color='indianred', label="Mean {0:.3f}".format(np.mean(base_rs)))
+	plt.vlines(0, -.25, 2.25, linestyles='--', color='darkorange', label="Perfect")
+
+	plt.title("% Of Customer Base Residuals")
+	plt.legend()
+	plt.show()
+	
 
 
 if __name__ == '__main__':
 	conn = connect()
 
 	ing_df = pd.read_csv('data/ingrds.csv')
-	user_objs = pickle.load(open('order_prediction/user_objects_small_dict.p', 'rb'))
+	user_objs = pickle.load(open('order_prediction/user_objects_test_dict.p', 'rb'))
 
 	ing_dict = dict(zip(ing_df.ingredient_id, ing_df.name))
 	ing_ids = list(ing_dict.keys())
 	ing_names = list(ing_dict.values())
 	
-	avg_rf_perror = []
-	avg_gb_perror = []
-	avg_perror = []
 
-	real = np.array([26,23,16,10])
-	meals = ['a050N00000zZg6AQAS','a050N00000zZg6BQAS',
-			 'a050N00000zZgH1QAK','a050N00000zZgH5QAK']
+	meal_groups = [['a050N00000zZg6AQAS','a050N00000zZg6BQAS','a050N00000zZgH1QAK','a050N00000zZgH5QAK'],
+				   ['a050N00000za4nlQAA','a050N00000za4nqQAA','a050N00000za4nvQAA','a050N00000za4o5QAA'],
+				   ['a050N000010W5ezQAC','a050N00000zZfyqQAC','a050N00000zZfyrQAC','a050N000010W5eyQAC'],
+				   ['a050N00000zbFdeQAE','a050N00000zbGCDQA2','a050N00000zbFdjQAE','a050N00000zbFdZQAU'],
+				   ['a050N00000zZgH2QAK','a050N00000zZgH4QAK','a050N00000zZgH3QAK','a050N00000zZgH7QAK'],
+				   ['a050N00000zbES4QAM','a050N00000zbESJQA2','a050N00000zZfz8QAC','a050N00000zbESEQA2'],
+				   ['a050N00000zZg5LQAS','a050N00000zZg5MQAS','a050N000010W5f1QAC','a050N000010W5f2QAC'],
+				   ['a050N000013OpNUQA0','a050N000010WrWZQA0','a050N000010WrWAQA0','a050N000010WrWKQA0'],
+				   ['a050N000010XyHYQA0','a050N000016uz5iQAA','a050N000010XyHCQA0','a050N000016uz5EQAQ'],
+				   ['a050N000014zW24QAE','a050N00001C0K0fQAF','a050N00001C0K7rQAF','a050N000014zW1aQAE'],
+				   ['a050N000010W5f8QAC','a050N000010W5f7QAC','a050N000010W5fAQAS','a050N000010W5f9QAC'],
+				   ['a050N00000zbES4QAM','a050N00000zbESJQA2','a050N00000zZfz8QAC','a050N00000zbESEQA2'],
+				   ['a050N00000zZg8AQAS','a050N000010WMcmQAG','a050N00000zZg8BQAS','a050N000010WMcrQAG']]
 
-	print("Set 1:")
-	ers = get_score(meals, user_objs, ing_ids, real)
-	avg_rf_perror.append(ers[0])
-	avg_gb_perror.append(ers[1])
-	avg_perror.append(ers[2])
-
-
-	real = np.array([14,39,23,22])
-	meals = ['a050N00000za4nlQAA','a050N00000za4nqQAA',
-			 'a050N00000za4nvQAA','a050N00000za4o5QAA']
-	
-	print("\nSet 2:")
-	ers = get_score(meals, user_objs, ing_ids, real)
-	avg_rf_perror.append(ers[0])
-	avg_gb_perror.append(ers[1])
-	avg_perror.append(ers[2])
+	real_ps_group = [np.array([26,23,16,10]), np.array([14,39,23,22]), np.array([42,26,47,71]),
+					 np.array([49,50,77,23]), np.array([25,6,14,30]), np.array([19,26,33,81]),
+					 np.array([71,21,13,5]), np.array([16,31,20,34]), np.array([25,38,43,15]),
+					 np.array([41,47,18,15]), np.array([40,28,10,8]), np.array([21,56,54,104]),
+					 np.array([71,40,56,28])]
 
 
+	avg_rf_perror, avg_gb_perror, avg_perror = [],[],[]
+	rf_resids, gb_resids, base_resids = [], [], []
+	for i in range(len(meal_groups)):
+		print("Set:", i)
+		ers, ps = get_score(meal_groups[i], user_objs, ing_ids, real_ps_group[i])
 
-	real = np.array([42,26,47,71])
-	meals = ['a050N000010W5ezQAC','a050N00000zZfyqQAC',
-			 'a050N00000zZfyrQAC','a050N000010W5eyQAC']
-	
-	print("\nSet 3:")
-	ers = get_score(meals, user_objs, ing_ids, real)
-	avg_rf_perror.append(ers[0])
-	avg_gb_perror.append(ers[1])
-	avg_perror.append(ers[2])
+		avg_rf_perror.append(ers[0])
+		avg_gb_perror.append(ers[1])
+		avg_perror.append(ers[2])
 
-
-	real = np.array([49,50,77,23])
-	meals = ['a050N00000zbFdeQAE','a050N00000zbGCDQA2',
-			 'a050N00000zbFdjQAE','a050N00000zbFdZQAU']
-	
-	print("\nSet 4:")
-	ers = get_score(meals, user_objs, ing_ids, real)
-	avg_rf_perror.append(ers[0])
-	avg_gb_perror.append(ers[1])
-	avg_perror.append(ers[2])
+		rf_resids.append(np.abs(ps[0]-ps[2]))
+		gb_resids.append(np.abs(ps[1]-ps[2]))
+		base_resids.append(np.abs(.25-ps[2]))
 
 
-	real = np.array([25,6,14,30])
-	meals = ['a050N00000zZgH2QAK','a050N00000zZgH4QAK',
-			 'a050N00000zZgH3QAK','a050N00000zZgH7QAK']
-	
-	print("\nSet 5:")
-	ers = get_score(meals, user_objs, ing_ids, real)
-	avg_rf_perror.append(ers[0])
-	avg_gb_perror.append(ers[1])
-	avg_perror.append(ers[2])
-
-
-
-	real = np.array([19,26,33,81])
-	meals = ['a050N00000zbES4QAM','a050N00000zbESJQA2',
-			 'a050N00000zZfz8QAC','a050N00000zbESEQA2']
-	
-	print("\nSet 6:")
-	ers = get_score(meals, user_objs, ing_ids, real)
-	avg_rf_perror.append(ers[0])
-	avg_gb_perror.append(ers[1])
-	avg_perror.append(ers[2])
-
-
-	real = np.array([71,21,13,5])
-	meals = ['a050N00000zZg5LQAS','a050N00000zZg5MQAS',
-			 'a050N000010W5f1QAC','a050N000010W5f2QAC']
-	
-	print("\nSet 7:")
-	ers = get_score(meals, user_objs, ing_ids, real)
-	avg_rf_perror.append(ers[0])
-	avg_gb_perror.append(ers[1])
-	avg_perror.append(ers[2])
-
-
-	real = np.array([11,30,18,23])
-	meals = ['a050N000010XxrMQAS','a050N000010XxrgQAC',
-			 'a050N000010XxrvQAC','a050N000010XxfVQAS']
-	
-	print("\nSet 8:")
-	ers = get_score(meals, user_objs, ing_ids, real)
-	avg_rf_perror.append(ers[0])
-	avg_gb_perror.append(ers[1])
-	avg_perror.append(ers[2])
 
 	print("\n----------------\nFINAL AVG ERRORS:")
-	print("RF error:", np.round(np.mean(avg_rf_perror), 3))
-	print("GB error:", np.round(np.mean(avg_gb_perror), 3))
-	print("Avg error:", np.round(np.mean(avg_perror), 3))
+	means = [np.round(np.mean(avg_rf_perror), 3),
+			 np.round(np.mean(avg_gb_perror), 3),
+			 np.round(np.mean(avg_perror), 3)]
+
+	print("RF error:", means[0])
+	print("GB error:", means[1])
+	print("Avg error:", means[2])
+
+
+	plot_swarm(rf_resids, gb_resids, base_resids)
 
 
 
