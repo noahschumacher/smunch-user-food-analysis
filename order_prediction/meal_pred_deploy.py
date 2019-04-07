77fd33c568ccf99e0ca35a12475f026263227cf1
Noah Schumacher <noahschumacher14@gmail.com>
2019-04-06 17:58:42 -0700

    Moved lots of files around, made deployable file for prediction.

--- order_prediction/meal_pred_deploy.py
+++ order_prediction/meal_pred_deploy.py
@@ -6,13 +6,35 @@ File:
 import numpy as np
 import pandas as pd
 import pickle
+import math
+
+import time
 
 def create_rows(meals, ing_ids):
+	'''
+	Function:
+	- creates the feature rows to be used in the model
+
+	Params:
+	------
+	- meals: list of lists where each inner list is a meal
+	and the values are the ingredients in the meal
+	- ing_ids: total list of ingredients in Smunch DB
+
+	Returns:
+	-------
+	- numpy 2D array of the feature rows (rows = n meals, cols = n tot ings)
+	'''
 
 	rows = []
-	for meal in meals:
-		ings = meal.ingredients
-		rows.append( np.array([ing in new_ings for ing in ing_ids]).astype(int) )
+	for meal in meals.keys():
+		meal_ings = meals[meal]
+
+		## Removing missing values caused by different length ingredients
+		meal_ings = [ing for ing in meal_ings if type(ing)==type('a')]
+
+		row = np.array([ing in meal_ings for ing in ing_ids]).astype(int)
+		rows.append(row)
 
 	## converting each meal to feature space row
 	return np.vstack(rows)
@@ -31,28 +53,73 @@ def get_preds(objs, rows):
 
 ## Getting user base breakdown
 def user_percent(preds, n_m):
+	'''
+	Function:
+	- Breaks down customer choices in % of total customers
+
+	Params:
+	------
+	- preds: 2d list of C-Score for each meal for each user
+	- n_m: int, number of meals
+
+	Returns:
+	------
+	- numpy 1d array with customer percents for each meal.
+	'''
+
 	counts = np.zeros(n_m)
 	for i in range(len(preds)):
 		sorts = np.argsort(preds[i])
 		counts[sorts[-1]] += 1
+
 	return np.round(np.array(counts)/sum(counts), 3)
 
 
 ## Run the functions
-def get_customer_percents(ing_dict, ing_ids, ing_names, user_objs, meals):
-	rows = create_rows(meals, ing_ids)
+def get_customer_percents(meal_ings_dict, tot_ingredient_ids, user_objs):
+	'''
+	Params:
+	-------
+	- meal_ingredients_l: dictionary of meal_id as key and values=list of ingrs
+	- tot_ingredient_ids: list of all ingredient ids in Smunch DB
+	- user_objs: dictionary with (key=user_id, val=user_object)
+
+	Returns:
+	------
+	- Numpy array with len n number of meals being compared
+	'''
+
+	rows = create_rows(meal_ings_dict, tot_ingredient_ids)
 	preds = get_preds(user_objs, rows)
-	user_percents = user_percent(preds, len(meals))
+	user_percents = user_percent(preds, len(meal_ings_dict))
 
-	return user_percent
+	return user_percents
 
 
-# ing_df = pd.read_csv('data/ingrds.csv')
-# ing_dict = dict(zip(ing_df.ingredient_id, ing_df.name))
-# ing_ids = list(ing_dict.keys())
-# ing_names = list(ing_dict.values())
 
-# user_objs = pickle.load(open('order_prediction/user_objects_deployable_dict.p', 'rb'))
+def run():
+	## Smunch Ingredient Info
+	ing_df = pd.read_csv('data/ingrds.csv')
+	ing_dict = dict(zip(ing_df.ingredient_id, ing_df.name))
+	ing_ids = list(ing_dict.keys())
+	ing_names = list(ing_dict.values())
+
+	## Getting the user objects
+	user_objs = pickle.load(open('order_prediction/user_objects_test_dict.p', 'rb'))
+
+	start = time.time()
+	## Reading in meals to compare to
+	meals = pd.read_csv('website/test_csvs/meal_comp1.csv')
+	meals.drop(['Unnamed: 0', 'Meals'], axis=1, inplace=True)
+	meals_d = meals.to_dict(orient='list')
+
+
+	print(get_customer_percents(meals_d, ing_ids, user_objs))
+	end = time.time()
+	print("Tot Time:", end-start)
+
+
+run()
 
 
 
--- order_prediction/user_master.py
+++ order_prediction/user_master.py
@@ -13,41 +13,10 @@ import numpy as np
 import pandas as pd
 import pickle
 
-import multiprocessing
 
+## local files annd user class
 from order_prediction.user_class import User
-
-## local run sql file
 from db.python_db import connect, run_sql_query
-
-
-def create_obj(u):
-	user_id, account_id, count = u
-	if count > 100:
-		print(u)
-		user = User(user_id, account_id, conn)
-		user.build_table()
-		return True, user
-		print('Added in obj')
-
-	else:
-		return False, None
-
-
-def run_users_p(users):
-	pool  = multiprocessing.Pool(4)
-
-	results = pool.map(create_obj, users)
-	user_objects = []
-
-	print("Here?")
-	for i, value in enumerate(results, start=users[0]):
-
-		if value[0]:
-			print("Added")
-			user_objects.append(value[1])
-
-	return user_objects
 
 
 
@@ -62,37 +31,44 @@ def sequential(user_acc_table, conn, sl=200):
 	Returns:
 	-------
 	- user_objects: dictionary with key as user_id and User object as value
+	- test: string idicating is test or deploy
 	'''
 
+	test = input("Test Model of Deploy (test/deploy): ")
 	sliced_users = user_acc_table.loc[user_acc_table.order_count > sl, :]
 
 	user_objects = {}
-
 	for i, user in enumerate(sliced_users.values):
 
 		print(user, "Building class...")
 		user_id, account_id, count = user
 
 		user = User(user_id, account_id)	## Create the object
-		user.build_table(conn, drop_tests=True)  ## Create user dictionary (targets and features)
-		user.build_model_test()			## Build RF and GB model for testing
-		#user.build_model_deploy()		## Build Deployable RF (no train/test)
+		user.build_table(conn, drop_tests=False)  ## Create user dictionary (targets and features)
+
+		if test == 'test':
+			user.build_model_test()			## Build RF and GB model for testing
+		else:
+			user.build_model_deploy()		## Build Deployable RF (no train/test)
 
 		user_objects[user_id] = user
 		print("Added", i+1)
 
-	return user_objects
+	return user_objects, test
 
 
 if __name__ == '__main__':
 	
 	conn = connect()
-	user_acc_table = pickle.load(open('order_prediction/user_account_order.p', 'rb'))
+	user_acc_table = pickle.load(open('order_prediction/pickles/user_account_order.p', 'rb'))
 
-	objs = sequential(user_acc_table, conn, 220)
-	pickle.dump(objs, open('order_prediction/user_objects_test_dict.p', 'wb'))
+	thresh = int(input("Set order threshold: "))
+	objs, test = sequential(user_acc_table, conn, thresh)
 
-	#run_users_p(sliced_users.values)
+	file = 'order_prediction/pickles/user_objects_'+test+'_dict.p'
+	pickle.dump(objs, open(file, 'wb'))
+
+
 
 
 
